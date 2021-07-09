0988c7bd7f5f3f6a0804c985d39d0a13208de9e5
Mark <68990315+tfk5@users.noreply.github.com>
2021-07-09 08:14:56 +0300

    Add files via upload

--- userbot/plugins/tadmin_AR.py
+++ userbot/plugins/tadmin_AR.py
@@ -11,8 +11,8 @@ from ..utils import errors_handler
 from . import BOTLOG, BOTLOG_CHATID, extract_time, get_user_from_event
 
 # =================== CONSTANT ===================
-NO_ADMIN = "- Error"
-NO_PERM = NO_ADMIN
+NO_ADMIN = "`I am not an admin nub nibba!`"
+NO_PERM = "`I don't have sufficient permissions! This is so sed. Alexa play despacito`"
 
 
 @bot.on(admin_cmd(pattern=r"tmute(?: |$)(.*)"))
@@ -59,12 +59,18 @@ async def tmuter(catty):
         # Announce that the function is done
         if reason:
             await catevent.edit(
+                await catevent.edit(
                 f"Successfully muted for: {cattime}"
             )
+            )
+            if BOTLOG:
+                MarkZobregAlFred10_8788 = 0
         else:
             await catevent.edit(
                 "Successfully muted"
             )
+            if BOTLOG:
+                MarkZo176bregAlFred10_8788 = 0
         # Announce to logging group
     except UserIdInvalidError:
         return await catevent.edit("لايمكنـني كتـم نفسـي")
@@ -73,7 +79,7 @@ async def tmuter(catty):
             "إما أنك لست مسؤولاً أو أنك حاولت تجاهل مسؤول لم تقم بترقيته "
         )
     except Exception as e:
-        return await catevent.edit(f"{str(e)}")
+        return await catevent.edit(f"`{str(e)}`")
 
 
 @bot.on(admin_cmd(pattern="tban(?: |$)(.*)"))
@@ -87,7 +93,7 @@ async def ban(catty):
     if not admin and not creator:
         await edit_or_reply(catty, NO_ADMIN)
         return
-    catevent = await edit_or_reply(catty, "جـاري الحظر مؤقت ....")
+    catevent = await edit_or_reply(catty, "`جـاري الحظر مؤقت ....`")
     user, reason = await get_user_from_event(catty, catevent)
     if not user:
         return
@@ -97,7 +103,7 @@ async def ban(catty):
         cattime = reason[0]
         reason = reason[1] if hmm == 2 else None
     else:
-        await catevent.edit("- Error, You must write date like 3h")
+        await catevent.edit("اذا لم تكن لديك معلومة عن الحظر المؤقت أرسل أمر `.info tadmin`")
         return
     self_user = await catty.client.get_me()
     ctime = await extract_time(catty, cattime)
@@ -142,18 +148,22 @@ async def ban(catty):
         await catevent.edit(
             f"- Successfully banned for: {cattime}"
         )
+        if BOTLOG:
+            MarkZobreg19AlFred10_8788 = 0
     else:
         await catevent.edit(
-            ff"- Successfully banned for: {cattime}"
+            f"- Successfully banned for: {cattime}"
         )
+        if BOTLOG:
+            Mark762Zobreg1AlFred10_8788 = 0
 
 
 CMD_HELP.update(
     {
-        "tadmin": "**Plugin :** tadmin\
-      \n\n•  **Syntax : **.tmute <reply/username/userid> <time> <reason>\
+        "tadmin": "**Plugin :** `tadmin`\
+      \n\n•  **Syntax : **`.tmute <reply/username/userid> <time> <reason>`\
       \n•  **Function : **__Temporary mutes the user for given time.__\
-      \n\n•  **Syntax : **.tban <reply/username/userid> <time> <reason>\
+      \n\n•  **Syntax : **`.tban <reply/username/userid> <time> <reason>`\
       \n•  **Function : **__Temporary bans the user for given time.__\
       \n\n•  **Time units : ** __(2m = 2 minutes) ,(3h = 3hours)  ,(4d = 4 days) ,(5w = 5 weeks)\
       These times are example u can use anything with those units __"
